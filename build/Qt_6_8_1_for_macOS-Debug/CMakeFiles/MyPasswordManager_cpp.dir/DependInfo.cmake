72cf96269618c37493acaa95f10b00d709f42879
pacodev30 <pacofustier@gmail.com>
2024-12-07 12:13:06 +0100

    Add class FileUtils

---
+++ build/Qt_6_8_1_for_macOS-Debug/CMakeFiles/MyPasswordManager_cpp.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/pascal/dev/cpp/MyPasswordManager_cpp/Entry.cpp" "CMakeFiles/MyPasswordManager_cpp.dir/Entry.cpp.o" "gcc" "CMakeFiles/MyPasswordManager_cpp.dir/Entry.cpp.o.d"
+  "/Users/pascal/dev/cpp/MyPasswordManager_cpp/Manager.cpp" "CMakeFiles/MyPasswordManager_cpp.dir/Manager.cpp.o" "gcc" "CMakeFiles/MyPasswordManager_cpp.dir/Manager.cpp.o.d"
+  "/Users/pascal/dev/cpp/MyPasswordManager_cpp/main.cpp" "CMakeFiles/MyPasswordManager_cpp.dir/main.cpp.o" "gcc" "CMakeFiles/MyPasswordManager_cpp.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Qt_6_8_1_for_macOS-Debug/CMakeFiles/MyPasswordManager_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyPasswordManager_cpp.dir/Entry.cpp.o"
+  "CMakeFiles/MyPasswordManager_cpp.dir/Entry.cpp.o.d"
+  "CMakeFiles/MyPasswordManager_cpp.dir/Manager.cpp.o"
+  "CMakeFiles/MyPasswordManager_cpp.dir/Manager.cpp.o.d"
+  "CMakeFiles/MyPasswordManager_cpp.dir/main.cpp.o"
+  "CMakeFiles/MyPasswordManager_cpp.dir/main.cpp.o.d"
+  "MyPasswordManager_cpp"
+  "MyPasswordManager_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyPasswordManager_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
